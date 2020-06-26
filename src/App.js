551c1f3aa187f659e34e845c73928b8226a3180b
byohannes <42332389+byohannes@users.noreply.github.com>
2020-06-26 03:09:42 +0100

    updated css style of the quotes block

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
-  const [randomQuote, setRandomQuote] = useState ({});
+  const [randomQuote, setRandomQuote] = useState({});
 
   const api_url = 'https://cyf--berhaneyohannes.repl.co/quotes/random';
-  const getQuote=()=>{
-    fetch (api_url)
-    .then (res => res.json ())
-    .then (data => setRandomQuote (data));
-  
-  }
-  useEffect (() => {
-      getQuote();
-    }, []);
+  const getQuote = () => {
+    fetch(api_url)
+      .then((res) => res.json())
+      .then((data) => setRandomQuote(data));
+  };
+  useEffect(() => {
+    getQuote();
+  }, []);
 
   return (
-    <div className="quote__block">
-      <h1 className="quote__text">{randomQuote.quote}</h1>
-      <h3 className="quote__author">- {randomQuote.author}</h3>
-      <button  className="quote__button" onClick={getQuote}>New Quote</button>
+    <div className="App">
+      <div className="quote__block">
+        <h1 className="quote__text">{randomQuote.quote}</h1>
+        <h3 className="quote__author">- {randomQuote.author}</h3>
+        <div className="quote__controls">
+          <button className="quote__button" onClick={getQuote}>
+            New Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
